e885f84b77e6b26c81596a611eb4ecfd4d801180
Allen Ruiz <allen.s.ruiz1@gmail.com>
2022-08-31 18:27:44 -0500

    Anniversary Flyer added to Modal

--- src/components/ThursdaySpecial.js
+++ src/components/ThursdaySpecial.js
@@ -1,39 +1,18 @@
 import './thursdayspecial.scss';
-// import secondPlace from '../images/secondPlace.jpg';
-// import firstPlace from '../images/firstPlace.jpg';
-// import thirdPlace from '../images/thirdPlace.jpg';
+
 
 function ThursdaySpecial() {
   return (
     <div className='mid-section'>
-      <div className='left-mid-section'>
-        {/* <h1>Congratulations</h1>
-        <h2>1st Annual 4th of July Cornhole Tournament Winners!!!</h2>
-        <span>
-          1st: Robert and Tyler
-          <br />
-          2nd: Sarah and Cameron
-          <br />
-          3rd: Bill and Anson
-          <br />
-          Raffle Winners: Holly won the Big Frig Cooler, Pam won the Apple
-          Crown!
-          <br />
-          Thank you everyone who came out! HAPPY FOURTH OF JULY!!
-        </span> */}
-        {/* <p> $12.00</p> */}
+      {/* <div className='left-mid-section'>
         <h2>Thursday Special</h2>
-        <br/>
+        <br />
         <p> 07/21/2022</p>
         <span>
-          Salisbury Steak, Gravy, Garlic Mashed Potatoes, Sweet Corn, Dinner Roll and Iced Tea
+          Salisbury Steak, Gravy, Garlic Mashed Potatoes, Sweet Corn, Dinner
+          Roll and Iced Tea
         </span>
         <p>$11.95</p>
-      </div>
-      {/* <div className='temp-section'>
-        <img src={firstPlace} alt='First Place' />
-        <img src={secondPlace} alt='Second Place' />
-        <img src={thirdPlace} alt='Third Place' />
       </div> */}
 
       <div className='right-mid-section'>
--- src/components/modals/Modal.js
+++ src/components/modals/Modal.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import cornHole from '../../images/cornhole.jpg';
+import anniversaryFlyer from '../../images/anniversaryFlyer.jpg';
 import './Modal.scss';
 
 const Modal = ({ open, onClose }) => {
@@ -13,28 +13,7 @@ const Modal = ({ open, onClose }) => {
         }}
         className='modalContainer'
       >
-        <img className='cornHoleimg' src={cornHole} alt='' />
-        <div className='modalRight'>
-          <p onClick={onClose} className='closeBtn'>
-            X
-          </p>
-          <div className='content'>
-            <h1>1st Annual 4th of July Cornhole Tournament</h1>
-            <h2>Saturday July 2nd, @4pm</h2>
-            <p>
-              Entry fee $10 per person. 1st place receives 70% of entry fees.
-              2nd place receives 30% of entry fees. 
-              <br/>
-              Call Gary anytime to signup (4323491538) or signup at Binnie Boys Burgers. 
-              Please signup before 3:30pm the day of the tournament.
-              <br/>
-              We will Raffle a 45 qt. Big Frig Cooler Donated by Kenney Building
-              Products. Tickets $5 each or 6 for $25.
-              <br/>
-              Bring Your Ice Chests!
-            </p>
-          </div>
-        </div>
+        <img className='anniversaryFlyer' src={anniversaryFlyer} alt='' />
       </div>
     </div>
   );
