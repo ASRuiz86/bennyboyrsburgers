11c8d829edfc61dfd83f659ba0b3f5dbefd7d8d4
Allen Ruiz <allen.s.ruiz1@gmail.com>
2023-11-12 10:16:20 -0600

    Remove Old Modal and add Burger Icon to Tab

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React, {useState} from 'react';
+// import React, {useState} from 'react';
 import Banner from './components/Banner';
 import menu from './images/NewMenu2022.jpg';
 import './App.css';
 import ThursdaySpecial from './components/ThursdaySpecial';
-import Modal from './components/modals/Modal';
+// import Modal from './components/modals/Modal';
 
 function App() {
-  const [openModal, setOpenModal] = useState(true);
+  // const [openModal, setOpenModal] = useState(true);
 
   return (
     <div>
-      <Modal open={openModal} onClose={() => setOpenModal(false)} />
+      {/* <Modal open={openModal} onClose={() => setOpenModal(false)} /> */}
       <Banner></Banner>
       <ThursdaySpecial />
       <img src={menu} alt='Menu' />
